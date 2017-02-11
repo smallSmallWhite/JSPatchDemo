d8687646ef3dfc97adbde7499be66526011a2215
admin <1395258209@qq.com>
2017-02-11 21:28:08 +0800

    创建一个项目

---
+++ JSPatchDemo/JSPatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSPatchDemo
+//
+//  Created by admin on 17/2/11.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSPatchDemo/JSPatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSPatchDemo
+//
+//  Created by admin on 17/2/11.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
